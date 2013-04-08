5b0b3efd59665b2449d9c310480b4242c08fe380
Jens Bertram <code@jens-bertram.net>
2013-04-08 16:24:14 +0200

    removed support for custom jessyink nodes

--- jessyInk.js
+++ jessyInk.js
@@ -317,9 +317,6 @@ function jessyInkInit() {
 	for (var counter = 0; counter < nodes.length; counter++) {
 		if (nodes[counter].getAttributeNS(NSS['inkscape'], "groupmode") && (nodes[counter].getAttributeNS(NSS['inkscape'], "groupmode") == "layer")) {
 			tempSlides.push(nodes[counter].getAttribute("id"));
-		} 
-		else if (nodes[counter].getAttributeNS(NSS['jessyink'], 'element')) {
-			handleElement(nodes[counter]);
 		}
 	}
 
@@ -2270,75 +2267,6 @@ function rectToMatrix(rect)
 	newVectors = (new matrixSVG()).fromElements(rectXcorr, rectXcorr + rectWidth, rectXcorr + rectWidth, rectYcorr, rectYcorr, rectYcorr + rectHeight, 0, 0, 0);
 
 	return mRectTrans.mult(newBasePoints.add(newVectors));
-}
-
-/** Function to handle JessyInk elements.
- *
- *	@param	node	Element node.
- */
-function handleElement(node)
-{
-	if (node.getAttributeNS(NSS['jessyink'], 'element') == 'core.video')
-	{
-		var url;
-		var width;
-		var height;
-		var x;
-		var y;
-		var transform;
-
-		var tspans = node.getElementsByTagNameNS("http://www.w3.org/2000/svg", "tspan");
-
-		for (var tspanCounter = 0; tspanCounter < tspans.length; tspanCounter++)
-		{
-			if (tspans[tspanCounter].getAttributeNS("https://launchpad.net/jessyink", "video") == "url")
-			{
-				url = tspans[tspanCounter].firstChild.nodeValue;
-			}
-		}
-
-		var rects = node.getElementsByTagNameNS("http://www.w3.org/2000/svg", "rect");
-
-		for (var rectCounter = 0; rectCounter < rects.length; rectCounter++)
-		{
-			if (rects[rectCounter].getAttributeNS("https://launchpad.net/jessyink", "video") == "rect")
-			{
-				x = rects[rectCounter].getAttribute("x");
-				y = rects[rectCounter].getAttribute("y");
-				width = rects[rectCounter].getAttribute("width");
-				height = rects[rectCounter].getAttribute("height");
-				transform = rects[rectCounter].getAttribute("transform");
-			}
-		}
-
-		for (var childCounter = 0; childCounter < node.childNodes.length; childCounter++)
-		{
-			if (node.childNodes[childCounter].nodeType == 1)
-			{
-				if (node.childNodes[childCounter].style)
-				{
-					node.childNodes[childCounter].style.display = 'none';
-				}
-				else
-				{
-					node.childNodes[childCounter].setAttribute("style", "display: none;");
-				}
-			}
-		}
-
-		var foreignNode = document.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
-		foreignNode.setAttribute("x", x);
-		foreignNode.setAttribute("y", y);
-		foreignNode.setAttribute("width", width);
-		foreignNode.setAttribute("height", height);
-		foreignNode.setAttribute("transform", transform);
-
-		var videoNode = document.createElementNS("http://www.w3.org/1999/xhtml", "video");
-		videoNode.setAttribute("src", url);
-
-		foreignNode.appendChild(videoNode);
-		node.appendChild(foreignNode);
-	}
 }
 
 /** Class processing the location hash.
